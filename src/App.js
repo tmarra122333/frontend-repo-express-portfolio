7a321d54dbf819d4821562375bfcbff3775eab7a
Tmarra122333 <Tmarra122333@gmail.com>
2022-03-01 20:56:15 -0500

    nick is the man

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+// IMPORT COMPONENTS
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+// IMPORT PAGES
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+function App() {
+  // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
+  const URL = "https://express-react-portfolio-marra.herokuapp.com/";
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects URL={URL} />} />
+        <Route path="/about" element={<About URL={URL} />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,5 @@
+function Footer(props) {
+    return <h1>Pizza and Tacos</h1>;
+  }
+  
+  export default Footer;
