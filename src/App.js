f2f8f5bf54c7e64bfcba48074693e3d98d861fc9
Tmarra122333 <Tmarra122333@gmail.com>
2022-03-03 16:35:31 -0500

    update 3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Projects from "./pages/Projects";
 
 function App() {
   // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
-  const URL = "https://express-react-portfolio-marra.herokuapp.com/";
+  const URL = "http://localhost:4000/";
 
   return (
     <div className="App">
