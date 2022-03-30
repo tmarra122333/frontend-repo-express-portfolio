c58cdcb341311eacbb7cc896963ebaf48c04453e
Tmarra122333 <Tmarra122333@gmail.com>
2022-03-30 19:33:49 -0400

    okay3

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,7 +23,7 @@ function About(props) {
       <h2>{about.name}</h2>
       <h3>{about.email}</h3>
       <p>{about.bio}</p>
-      <img className="PhotosBooBoo" src={about.headshot} alt="" />  
+      <img className="PhotosBooBoo2" src={about.headshot} alt="" />  
     </div>
   );
 
